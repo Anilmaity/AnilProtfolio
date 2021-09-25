75b9d81cd1c7fdf11aa0d713c49df7b5a69319fa
Anil <anilmaity18@gmail.com>
2021-09-25 14:53:45 +0530

    testing achyut code

--- AnilProtfolio/settings.py
+++ AnilProtfolio/settings.py
@@ -18,7 +18,6 @@ import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -40,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Projects',
 ]
 
 MIDDLEWARE = [
@@ -124,6 +124,9 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_DIRS = [
+    print(os.path.join(BASE_DIR, 'static')),
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type

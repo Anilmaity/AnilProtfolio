8dffb3775cdfda4521e669fff78b81100503f49e
Anil <anilmaity18@gmail.com>
2021-12-19 21:31:06 +0530

    files removed

--- AnilProtfolio/settings.py
+++ AnilProtfolio/settings.py
@@ -122,9 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-print(STATIC_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

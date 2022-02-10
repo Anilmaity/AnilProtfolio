14e698a3d73252a9a48db60db5d8d88d7795d55a
Anil <anilmaity18@gmail.com>
2022-02-10 19:51:20 +0530

    update requirements.txt

--- AnilProtfolio/settings.py
+++ AnilProtfolio/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 # Default primary key field type
--- Procfile
+++ Procfile
@@ -1,3 +1,5 @@
 python manage.py collectstatic --noinput
 web: gunicorn AnilProtfolio.wsgi --log-file -
 manage.py migrate
+
+
